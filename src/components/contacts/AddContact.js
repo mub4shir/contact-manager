b79d42dded9017dc15079ac6a7693dce31f23f35
mubashir <mubashir11131719@gmail.com>
2021-10-14 02:00:51 +0530

    refactor reqs async await

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -11,7 +11,7 @@ class AddContact extends Component {
     phone: '',
     errors: {},
   };
-  onSubmit = (dispatch, e) => {
+  onSubmit = async (dispatch, e) => {
     e.preventDefault();
     const { name, email, phone } = this.state;
     // Check For Errors
@@ -32,10 +32,12 @@ class AddContact extends Component {
       email,
       phone,
     };
-    axios
-      .post('https://jsonplaceholder.typicode.com/users', newContact)
-      .then((res) => dispatch({ type: 'ADD_CONTACT', payload: res.data }));
+    const res = await axios.post(
+      'https://jsonplaceholder.typicode.com/users',
+      newContact
+    );
 
+    dispatch({ type: 'ADD_CONTACT', payload: res.data });
     // Clear State
     this.setState({
       name: '',
